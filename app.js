2ebf1a2e96ba7a6d856df3d921d13095139543f9
Robinson Simon <cf.youvarsity@gmail.com>
2023-08-27 01:49:41 +0100

    added new route

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ const verifyUser = require("./middlewares/authorization");
 const errorHandler = require("./middlewares/errorHandler");
 
 const port = 3000;
-app.get("/", (req, res) => res.send("Hello World!"));
 
 //middlewares
 app.use(express.json());
 app.use("/api/v1", authRouter);
 
-app.use(notFoundMiddleware);
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
   res.send("TACSFON Web API");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandler);
+
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
