ae8379140dcc4b31d319eac700ba241dd276f1fa
Robinson Simon <cf.youvarsity@gmail.com>
2023-08-30 03:09:23 +0100

    repositioned cors middleware

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-//CORS
-app.use(cors());
-
 //Routers
 const authRouter = require("./routes/auth");
 const excoRouter = require("./routes/excoRoute");
@@ -32,6 +29,12 @@ const verifyUser = require("./middlewares/authorization");
 const port = 3000;
 
 //middlewares
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 
